f0e65d1abe035a2ecb6c6020997aefec7bccde5f
zhaoliuxue <2096343460@qq.com>
2026-05-08 12:23:56 +0800

    123

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 81,
     proxy: {
-      '/tts': 'http://127.0.0.1:8080'
+      '/tts': {
+        target: 'http://127.0.0.1:8080',
+        rewrite: (path) => path.replace(/^\/tts/, '')
+      }
     }
   },
   preview: {
